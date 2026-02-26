ef113bbeb61642f5df6b873783342e054d913354
Amitoj Singh <amitojsingh95@gmail.com>
2026-02-26 10:35:57 -0700

    Enhance medusa-config.ts by adding S3 file provider configuration and updating import syntax for consistency.

--- medusa-config.ts
+++ medusa-config.ts
@@ -1,6 +1,6 @@
-import { loadEnv, defineConfig } from '@medusajs/framework/utils'
+import { loadEnv, defineConfig } from "@medusajs/framework/utils";
 
-loadEnv(process.env.NODE_ENV || 'development', process.cwd())
+loadEnv(process.env.NODE_ENV || "development", process.cwd());
 
 module.exports = defineConfig({
   projectConfig: {
@@ -11,6 +11,28 @@ module.exports = defineConfig({
       authCors: process.env.AUTH_CORS!,
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-    }
-  }
-})
+    },
+  },
+  modules: [
+    {
+      resolve: "@medusajs/medusa/file",
+      options: {
+        providers: [
+          {
+            resolve: "@medusajs/medusa/file-s3",
+            id: "s3",
+            options: {
+              file_url: process.env.AWS_FILE_URL,
+              access_key_id: process.env.AWS_ACCESS_KEY_ID,
+              secret_access_key: process.env.AWS_SECRET_ACCESS_KEY,
+              region: process.env.AWS_DEFAULT_REGION,
+              bucket: process.env.AWS_S3_BUCKET_NAME,
+              endpoint: process.env.AWS_ENDPOINT_URL,
+              // other options...
+            },
+          },
+        ],
+      },
+    },
+  ],
+});
